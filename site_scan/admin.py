170270be7ceccd009b6a3a10b667310b9af96d94
Shariq Jamil <shariq1989@gmail.com>
2024-11-27 16:21:24 -0500

    add board suggest to admin

--- site_scan/admin.py
+++ site_scan/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Site, BlogPost
+from .models import Site, BlogPost, PinterestBoardSuggestion
 
 
 class SiteAdmin(admin.ModelAdmin):
@@ -8,8 +8,29 @@ class SiteAdmin(admin.ModelAdmin):
 
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = (
-    "site", "title", "description", "url", "featured_image_url", "pin_image_url", "created_on", "date_modified")
+        "site", "title", "description", "url", "featured_image_url", "pin_image_url", "created_on", "date_modified")
+
+
+class PinterestBoardSuggestionAdmin(admin.ModelAdmin):
+    list_display = (
+        'blog_post',
+        'board_name',
+        'board_id',
+        'match_score',
+        'is_selected',
+        'created_at'
+    )
+    list_filter = (
+        'is_selected',
+        'created_at'
+    )
+    search_fields = (
+        'blog_post__title',
+        'board_name'
+    )
+    readonly_fields = ('created_at',)
 
 
 admin.site.register(Site, SiteAdmin)
 admin.site.register(BlogPost, BlogPostAdmin)
+admin.site.register(PinterestBoardSuggestion, PinterestBoardSuggestionAdmin)
